b300a40a7c671c1791bb7237b9f400cd03f0e716
melvin <melvinrf@outlook.com>
2021-09-22 14:00:34 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,53 +1,28 @@
 # Employee Tracker
 
-## Description
+![GitHub license](https://img.shields.io/badge/license-MIT-red.svg)
 
-Employee tracker is an application that lets a user manage departments, employees and roles.
+# Description
 
-## Video
+Employee tracker is a terminal application that lets a user manage departments, employees and roles.
 
-[![Homework Assignment](https://www.kapwing.com/videos/60d2a3596c87d500529cde71)](https://www.kapwing.com/videos/60d2a3596c87d500529cde71)
+# Usage
 
-## Table of Contents
+```
+# Clone the repo
+$ git clone git@github.com:campe0n/employeetracker.git
 
-[Description](#description)
+# Run the eTracker.js file
+$ node eTracker.js
 
-[Video](#video)
+# Or run with nodemon
+$ nodemon eTracker.js
+```
 
-[Installation Instructions](#installation-instructions)
-
-[Usage Information](#usage-information)
-
-[Contribution Guidelines](#contribution-guidelines)
-
-[Test Instructions](#test-instructions)
-
-[Questions](#questions)
-
-## Installation Instructions
-
-No installation instructions.
-
-## Usage Information
-
-run node index.js to start the application
-
-## Contribution Guidelines
-
-No contribution is neccessary.
-
-## Test Instructions
-
-No test instructions.
-
-## Questions
+# Questions
 
 If you have any questions, you can reach me using these methods.
 
-[github](https://github.com/campe0n)
+Github: [Github](https://github.com/campe0n)
 
-email: melvinrf@outlook.com
-
-## License
-
-This project is licensed by MIT
+Email: melvinrf@outlook.com
--- eTracker.js
+++ eTracker.js
@@ -1,227 +1,233 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer')
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'employeeDB',
+  host: "localhost",
+  port: 3001,
+  user: "root",
+  password: "password",
+  database: "employeeDB",
 });
 
 const promptUser = () => {
-    inquirer.prompt({
-        type: 'list',
-        message: `What would you like to do?`,
-        name: 'list',
-        choices: [
-            'Add a department',
-            'Add a role',
-            'Add an employee' ,
-            'View departments',
-            'View roles', 
-            "View employee's",
-            'Update employee roles',
-            'Exit'
-        ]})
-        .then((answer) => {
-          // based on their answer, either call the bid or the post functions
-          if (answer.list === 'Add a department') {
-            addDepartment();
-          } else if (answer.list === 'Add a role') {
-            addRole();
-          } else if (answer.list === 'Add an employee') {
-            addEmployee();
-          } else if (answer.list === 'View departments') {
-            viewDepartments();
-          } else if (answer.list === 'View roles') {
-            viewRoles();
-          } else if (answer.list === "View employee's") {
-            viewEmployees();
-          } else if (answer.list === "Update employee roles") {
-            updateRoles();
-          } else {
-            connection.end();
-          }
-        });
-    };
+  inquirer
+    .prompt({
+      type: "list",
+      message: `What would you like to do?`,
+      name: "list",
+      choices: [
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "View departments",
+        "View roles",
+        "View employee's",
+        "Update employee roles",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      // based on their answer, either call the bid or the post functions
+      if (answer.list === "Add a department") {
+        addDepartment();
+      } else if (answer.list === "Add a role") {
+        addRole();
+      } else if (answer.list === "Add an employee") {
+        addEmployee();
+      } else if (answer.list === "View departments") {
+        viewDepartments();
+      } else if (answer.list === "View roles") {
+        viewRoles();
+      } else if (answer.list === "View employee's") {
+        viewEmployees();
+      } else if (answer.list === "Update employee roles") {
+        updateRoles();
+      } else {
+        connection.end();
+      }
+    });
+};
 
 const addDepartment = () => {
-        inquirer
-          .prompt([
-            {
-              name: 'department',
-              type: 'input',
-              message: 'What is the name of the department you want to add?',
-            }
-          ])
-          .then((answer) => {
-            connection.query(
-              'INSERT INTO department SET ?',
-              {
-                name: answer.department,
-              },
-              (err) => {
-                if (err) throw err;
-                console.log('Department was added succesfully!');
-                promptUser();
-              }
-            );
-          });
-      };
+  inquirer
+    .prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "What is the name of the department you want to add?",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO department SET ?",
+        {
+          name: answer.department,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Department was added succesfully!");
+          promptUser();
+        }
+      );
+    });
+};
 
 const addRole = () => {
-        inquirer
-          .prompt([
-            {
-              name: 'role',
-              type: 'input',
-              message: 'What is the name of the role you want to add?',
-            },
-            {
-              name: 'salary',
-              type: 'input',
-              message: 'What is the salary of this role?'
-            },
-          ])
-          .then((answer) => {
-            connection.query(
-              'INSERT INTO role SET ?',
-              {
-                title: answer.role,
-                salary: answer.salary,
-              },
-              (err) => {
-                if (err) throw err;
-                console.log('Role was added succesfully!');
-                promptUser();
-              }
-            );
-          });
-      };
-      
+  inquirer
+    .prompt([
+      {
+        name: "role",
+        type: "input",
+        message: "What is the name of the role you want to add?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary of this role?",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO role SET ?",
+        {
+          title: answer.role,
+          salary: answer.salary,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Role was added succesfully!");
+          promptUser();
+        }
+      );
+    });
+};
+
 const addEmployee = () => {
-        inquirer
-          .prompt([
-            {
-              name: 'fName',
-              type: 'input',
-              message: 'What is the fist name of the employee?',
-            },
-            {
-              name: 'lName',
-              type: 'input',
-              message: 'What is the last name of the employee?',
-            },
-            {
-              name: 'roleid',
-              type: 'input',
-              message: 'What is the role id of the employee?',
-            },
-            {
-              name: 'managerid',
-              type: 'input',
-              message: "What is the id of the employee's manager?",
-            },
-          ])
-          .then((answer) => {
-            connection.query(
-              'INSERT INTO employee SET ?',
-              {
-                first_name: answer.fName,
-                last_name: answer.lName,
-                role_id: answer.roleid,
-                manager_id: answer.managerid,
-              },
-              (err) => {
-                if (err) throw err;
-                console.log('Employee was added succesfully!');
-                promptUser();
-              }
-            );
-          });
-      };
+  inquirer
+    .prompt([
+      {
+        name: "fName",
+        type: "input",
+        message: "What is the fist name of the employee?",
+      },
+      {
+        name: "lName",
+        type: "input",
+        message: "What is the last name of the employee?",
+      },
+      {
+        name: "roleid",
+        type: "input",
+        message: "What is the role id of the employee?",
+      },
+      {
+        name: "managerid",
+        type: "input",
+        message: "What is the id of the employee's manager?",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: answer.fName,
+          last_name: answer.lName,
+          role_id: answer.roleid,
+          manager_id: answer.managerid,
+        },
+        (err) => {
+          if (err) throw err;
+          console.log("Employee was added succesfully!");
+          promptUser();
+        }
+      );
+    });
+};
 
 const viewDepartments = () => {
-        console.log('Showing all departments...\n');
-          connection.query('SELECT * FROM department', (err, res) => {
-            if (err) throw err;
-            // Log all results of the SELECT statement
-            console.table(res);
-            promptUser();
-          })
-        }
-      
+  console.log("Showing all departments...\n");
+  connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    promptUser();
+  });
+};
+
 const viewRoles = () => {
-        console.log('Showing all roles...\n');
-          connection.query('SELECT * FROM role', (err, res) => {
-            if (err) throw err;
-            // Log all results of the SELECT statement
-            console.table(res);
-            promptUser();
-          })
-        }
+  console.log("Showing all roles...\n");
+  connection.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    promptUser();
+  });
+};
 
 const viewEmployees = () => {
-            console.log("Showing all employee's...\n");
-              connection.query('SELECT * FROM employee', (err, res) => {
-                if (err) throw err;
-                // Log all results of the SELECT statement
-                console.table(res);
-                promptUser();
-              })
-            }
+  console.log("Showing all employee's...\n");
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    promptUser();
+  });
+};
 
 const updateRoles = () => {
-            connection.query("select r.id,r.title,e.id,e.first_name,e.last_name, e.role_id from role as r Inner JOIN  employee as e on r.id = e.role_id order by r.id",(err, results) => {
-            inquirer.prompt([
-                {
-                    name: 'choice',
-                    type: 'rawlist',
-                    message: 'Which employee would you like to have their role updated?',
-                    choices() {
-                      var choiceArray = [];
-                      results.forEach(({first_name}) => {
-                        choiceArray.push(first_name);
-                      });
-                      return choiceArray;
-                    },
-                },
-                {
-                    name: 'newRole',
-                    type: 'rawlist',
-                    message: "What job would you like to change to?",
-                    choices() {
-                      var numberArray = [];
-                      results.forEach(({id}) => {
-                        numberArray.push(id);
-                      });   
-                      console.log(numberArray);
-                      return numberArray;
-                    }
-                },
-            ])
-            .then((answer) => {
-                connection.query(
-                    'UPDATE employee SET ? WHERE ?',
-                    [
-                      {
-                        role_id: answer.newRole,
-                      },
-                      {
-                        id: answer.choice,
-                      },
-                    ],
-                    (error) => {
-                      if (error) throw err;
-                      console.log('Employee role updated successfully!');
-                      promptUser();
-                    }
-                );
-            });
+  connection.query(
+    "select r.id,r.title,e.id,e.first_name,e.last_name, e.role_id from role as r Inner JOIN  employee as e on r.id = e.role_id order by r.id",
+    (err, results) => {
+      inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "rawlist",
+            message:
+              "Which employee would you like to have their role updated?",
+            choices() {
+              var choiceArray = [];
+              results.forEach(({ first_name }) => {
+                choiceArray.push(first_name);
+              });
+              return choiceArray;
+            },
+          },
+          {
+            name: "newRole",
+            type: "rawlist",
+            message: "What job would you like to change to?",
+            choices() {
+              var numberArray = [];
+              results.forEach(({ id }) => {
+                numberArray.push(id);
+              });
+              console.log(numberArray);
+              return numberArray;
+            },
+          },
+        ])
+        .then((answer) => {
+          connection.query(
+            "UPDATE employee SET ? WHERE ?",
+            [
+              {
+                role_id: answer.newRole,
+              },
+              {
+                id: answer.choice,
+              },
+            ],
+            (error) => {
+              if (error) throw err;
+              console.log("Employee role updated successfully!");
+              promptUser();
+            }
+          );
         });
-    };
-            
+    }
+  );
+};
 
 connection.connect((err) => {
   if (err) throw err;
